f409abcfab6d8650ca1b721c9a4cf108b9736f1c
pritam3339 <pdeypritam@gmail.com>
2020-01-08 13:24:57 +0530

    fitting model

---
+++ model fit.R
@@ -0,0 +1,14 @@
+library(lattice)
+df <- read.csv("data/data.csv")
+
+df$logn = log(df$n)
+df$t_n = df$t / df$n
+
+model_logn <- lm(t_n ~ logn, data = df)
+summary(model_logn)
+
+fitted_logn = predict(model_logn)
+
+xyplot(t_n + fitted_logn ~ n, data = df,
+       type=c('p', 'l'),
+       auto.key = TRUE)
--- simulation.R
+++ simulation.R
@@ -1,12 +1,12 @@
-library(lattice)
 library(microbenchmark)
 
 avg <- function(n, r) {
   y = numeric(0)
   for (i in 1:r){
-    s = runif(n)
-    w = floor(runif(1) * n) + 1
-    t = summary(microbenchmark(order_statistic(s, w), times = 1L))[["mean"]]
+    s = sample(1:n)
+    #w = floor(runif(1) * n) + 1
+    w = 700
+    t = summary(microbenchmark(order_statistic(s, w), times = 1L, unit = "us"))[["mean"]]
     y = c(y, t)
   }
   mean(y)
@@ -14,7 +14,6 @@ avg <- function(n, r) {
 
 test_hyp <- function(N = 20000, r=20) {
   n = seq(1000, N, by = 1000)
-  nlogn = sapply(n, function (k) k*log(k))
   t = rep(0, length(n))
   j = 1
   for (i in n){
@@ -22,12 +21,10 @@ test_hyp <- function(N = 20000, r=20) {
     print(j) #to track progress
     j=j+1
   }
-  xyplot(t ~ n,
-         type=c('p', 'l', 'smooth'),
-         auto.key = TRUE)
+  write.csv(df, "data.csv")
+  df
 }
 
-test_hyp() #the loes curve is straight line hence average case run time is O(n)
-# from the curve we see the variance is also increasing with increase in n
-# since it is not increasing very reapidly, we guess order of variance is also
-# O(n)
+df = test_hyp()
+
+
